2190580011b671909da373453a513da59a6173e4
C-SideStep <brotfan.mh@gmail.com>
2017-09-02 20:36:10 +0200

    Update rs.js

--- rs.js
+++ rs.js
@@ -9,8 +9,8 @@ function setup() {
 function draw_cC(fC, speed){
   fill("black");
   rect(0,0,500,500);
-  fill("green");
-  stroke("green");
+  fill("red");
+  stroke("red");
   ellipse(250 + sin(((fC) % 360*speed)) * 100, 250 + cos(((fC) % 360*speed))  * 100, 10 , 10);
   ellipse(250 + sin(90 + ((fC) % 360*speed)) * 100, 250 + cos(90 + ((fC) % 360*speed))  * 100, 10 , 10);
   ellipse(250 + sin(180 + ((fC) % 360*speed)) * 100, 250 + cos(180 + ((fC) % 360*speed))  * 100, 10 , 10);
